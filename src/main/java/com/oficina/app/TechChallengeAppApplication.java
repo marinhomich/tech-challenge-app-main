c805bbaf4c4ebe4c39c778a2b6cd7ab1eea8ce5f
Michel Marinho <marinhomich@gmail.com>
2026-04-02 12:46:08 -0400

    Initial Commit

---
+++ src/main/java/com/oficina/app/TechChallengeAppApplication.java
@@ -0,0 +1,13 @@
+package com.oficina.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oficina/app/TechChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficina.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
