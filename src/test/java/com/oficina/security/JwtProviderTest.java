f09fcbd0d92faa3dc63a43da40c37c526f866012
Michel Marinho <marinhomich@gmail.com>
2026-04-20 23:57:31 -0400

    fix: Construtor do JwtProvider na classe de teste

--- src/test/java/com/oficina/security/JwtProviderTest.java
+++ src/test/java/com/oficina/security/JwtProviderTest.java
@@ -8,8 +8,7 @@ class JwtProviderTest {
 
     @Test
     void testValidateToken_Invalid() {
-        JwtProvider provider = new JwtProvider();
-        ReflectionTestUtils.setField(provider, "jwtSecret", "minha_chave_super_secreta_padrao_para_desenvolvimento_local_de_mais_de_256_bits_123");
+        JwtProvider provider = new JwtProvider("minha_chave_super_secreta_padrao_para_desenvolvimento_local_de_mais_de_256_bits_123");
         assertFalse(provider.validateToken("invalidToken"));
     }
 }
